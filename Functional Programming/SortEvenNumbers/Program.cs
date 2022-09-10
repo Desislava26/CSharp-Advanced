94a09f83c80eeba3684fa1933821680d135458a0
Desislava26 <106395233+Desislava26@users.noreply.github.com>
2022-09-10 12:13:11 +0300

    Add files via upload

---
+++ Functional Programming/SortEvenNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortEvenNumbers
+{
+    internal class test
+    {
+        static void Main()
+        {
+           int [] numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+
+            Func<int, bool> func = x => x;
+           
+        }
+    }
+}
